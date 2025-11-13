adba5d9a6d252b78b52ad52d8c6cc46d07e5cc1f
rfsaldanha <rfsaldanha@gmail.com>
2025-11-13 15:56:47 -0300

    update

--- 01_utci_download.R
+++ 01_utci_download.R
@@ -15,51 +15,51 @@ library(fs)
 wf_set_key(key = Sys.getenv("era5_API_Key"))
 
 # Parameters
-dir_data <- "ntci_data/"
-years <- "2024"
-month <- str_pad(string = 1:12, width = 2, pad = "0")
+dir_data <- "/media/raphaelsaldanha/lacie/clim"
+years <- as.character(2024:1940)
+month <- str_pad(string = 12:1, width = 2, pad = "0")
 day <- str_pad(string = 1:31, width = 2, pad = "0")
 
 # Download loop
 for (y in years) {
-  for(m in months){
+  for (m in month) {
     # File name
-  file_name <- glue(
-    "clim_ntci_{y}.nc"
-  )
+    file_name <- glue(
+      "clim_ntci_{y}.nc"
+    )
 
-  # Check if file is already available
-  if (file.exists(paste0(dir_data, "/", file_name))) {
-    cli_alert_warning("File already exists. Going for next.")
-    next
-  }
+    # Check if file is already available
+    if (file.exists(paste0(dir_data, "/", file_name))) {
+      cli_alert_warning("File already exists. Going for next.")
+      next
+    }
 
-  # Declare request
-  request <- list(
-    dataset_short_name = "derived-utci-historical",
-    variable = "universal_thermal_climate_index",
-    version = "1_1",
-    product_type = "consolidated_dataset",
-    year = y,
-    month = m,
-    day = day,
-    data_format = "netcdf",
-    download_format = "unarchived",1
-    # area = c(33.28, -118.47, -56.65, -34.1),
-    target = file_name
-  )
+    # Declare request
+    request <- list(
+      dataset_short_name = "derived-utci-historical",
+      variable = "universal_thermal_climate_index",
+      version = "1_1",
+      product_type = "consolidated_dataset",
+      year = y,
+      month = m,
+      day = day,
+      data_format = "netcdf",
+      download_format = "unarchived",
+      # area = c(33.28, -118.47, -56.65, -34.1),
+      target = file_name
+    )
 
-  # Download file with retry
-  retry(
-    expr = {
-      wf_request(
-        request = request,
-        transfer = TRUE,
-        path = dir_data
-      )
-    },
-    interval = 1,
-    until = ~ is_file(as.character(.))
-  )
+    # Download file with retry
+    retry(
+      expr = {
+        wf_request(
+          request = request,
+          transfer = TRUE,
+          path = dir_data
+        )
+      },
+      interval = 1,
+      until = ~ is_file(as.character(.))
+    )
   }
 }
